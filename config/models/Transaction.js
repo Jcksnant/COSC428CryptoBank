aeb0759c3d5f5737a22683d44248baed4f0f4763
Jcksnant <ant04jack@gmail.com>
2026-05-12 18:02:04 -0400

    Delete config/models/Transaction.js

--- config/models/Transaction.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("Transaction", {
-        type: DataTypes.STRING,
-        amount: DataTypes.FLOAT,
-        status: DataTypes.STRING,
-        hash: DataTypes.STRING
-    });
-};
